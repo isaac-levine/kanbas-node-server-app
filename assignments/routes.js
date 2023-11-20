44c4acf7f0bed2034199f752a4e2f5738db365b5
Isaac Levine <isaacmlevine4@gmail.com>
2023-11-19 19:37:32 -0500

    connected assignments routes to app.js

--- assignments/routes.js
+++ assignments/routes.js
@@ -3,20 +3,20 @@ import db from "../Database/index.js";
 function AssignmentRoutes(app) {
   // route for getting all assignments
   app.get("/api/assignments", (req, res) => {
-    res.send(Database.assignments);
+    res.send(db.assignments);
   });
 
   // route for creating a new assignment
   app.post("/api/courses/:cid/assignments", (req, res) => {
     const assignment = { ...req.body, _id: new Date().getTime().toString() };
-    Database.assignments.push(assignment);
+    db.assignments.push(assignment);
     res.send(assignment);
   });
 
   // route for getting all assignments of this course
   app.get("/api/courses/:cid/assignments", (req, res) => {
     const { cid } = req.params;
-    const assignments = Database.assignments.filter(
+    const assignments = db.assignments.filter(
       (assignment) => assignment.course === cid
     );
     res.send(assignments);
@@ -25,7 +25,7 @@ function AssignmentRoutes(app) {
   // route for getting a specific assignment
   app.get("/api/courses/:cid/assignments/:aid", (req, res) => {
     const { aid } = req.params;
-    const assignment = Database.assignments.find(
+    const assignment = db.assignments.find(
       (assignment) => assignment._id === aid
     );
     if (!assignment) {
@@ -38,7 +38,7 @@ function AssignmentRoutes(app) {
   // route for deleting an assigment
   app.delete("/api/courses/:cid/assignments/:aid", (req, res) => {
     const { aid } = req.params;
-    Database.assignments = Database.assignments.filter(
+    db.assignments = db.assignments.filter(
       (assignment) => assignment._id !== aid
     );
     res.sendStatus(200);
@@ -47,11 +47,11 @@ function AssignmentRoutes(app) {
   // route for updating an assigment
   app.put("/api/courses/:cid/assignments/:aid", (req, res) => {
     const { aid } = req.params;
-    const assignmentIndex = Database.assignments.findIndex(
+    const assignmentIndex = db.assignments.findIndex(
       (assignment) => assignment._id === aid
     );
-    Database.assignments[assignmentIndex] = {
-      ...Database.assignments[assignmentIndex],
+    db.assignments[assignmentIndex] = {
+      ...db.assignments[assignmentIndex],
       ...req.body,
     };
     res.sendStatus(204);
