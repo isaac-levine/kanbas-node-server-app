3b1298b6cbe5e645fafb260b21bc341d732096fe
Isaac Levine <isaacmlevine4@gmail.com>
2023-11-19 19:16:54 -0500

    added assignment routes

---
+++ assignments/routes.js
@@ -0,0 +1,55 @@
+import Database from "../Database";
+function AssignmentRoutes(app) {
+  // route for creating a new assignment
+  app.post("/api/courses/:cid/assignments", (req, res) => {
+    const assignment = { ...req.body, _id: new Date().getTime().toString() };
+    Database.assignments.push(assignment);
+    res.send(assignment);
+  });
+
+  // route for getting all assignments of this course
+  app.get("/api/courses/:cid/assignments", (req, res) => {
+    const { cid } = req.params;
+    const assignments = Database.assignments.filter(
+      (assignment) => assignment.course === cid
+    );
+    res.send(assignments);
+  });
+
+  // route for getting a specific assignment
+  app.get("/api/courses/:cid/assignments/:aid", (req, res) => {
+    const { aid } = req.params;
+    const assignment = Database.assignments.find(
+      (assignment) => assignment._id === aid
+    );
+    if (!assignment) {
+      res.status(404).send("Assignment not found");
+      return;
+    }
+    res.send(assignment);
+  });
+
+  // route for deleting an assigment
+  app.delete("/api/courses/:cid/assignments/:aid", (req, res) => {
+    const { aid } = req.params;
+    Database.assignments = Database.assignments.filter(
+      (assignment) => assignment._id !== aid
+    );
+    res.sendStatus(200);
+  });
+
+  // route for updating an assigment
+  app.put("/api/courses/:cid/assignments/:aid", (req, res) => {
+    const { aid } = req.params;
+    const assignmentIndex = Database.assignments.findIndex(
+      (assignment) => assignment._id === aid
+    );
+    Database.assignments[assignmentIndex] = {
+      ...Database.assignments[assignmentIndex],
+      ...req.body,
+    };
+    res.sendStatus(204);
+  });
+}
+
+export default AssignmentRoutes;
