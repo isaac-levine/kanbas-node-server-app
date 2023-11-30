6734766accaada137bc526a7ffca0c0944738f1c
Isaac Levine <isaacmlevine4@gmail.com>
2023-11-30 12:24:57 -0500

    stuff

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// const express = require("express");
 import express from "express";
 import HelloRoutes from "./hello.js";
 import Lab5 from "./Lab5.js";
@@ -8,18 +7,33 @@ import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
+import "dotenv/config";
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
-
 app.use(
   cors({
     credentials: true,
     origin: process.env.FRONTEND_URL,
   })
 );
+app.use(express.json());
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(session(sessionOptions));
 
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
@@ -28,6 +42,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 UserRoutes(app);
 Lab5(app);
 app.use(express.json());
+
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
